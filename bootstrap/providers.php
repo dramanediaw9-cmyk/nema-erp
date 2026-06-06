466ef81531d7a1c7a184b32d9de0057ab6f1283f
dramanediaw9-cmyk <dramanediaw9@gmail.com>
2026-06-06 22:53:15 +0000

    Active l’espace Mon compte

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,11 +1,13 @@
 <?php
 
 use App\Providers\AppServiceProvider;
+use App\Providers\AccountProfileServiceProvider;
 use App\Providers\AccountSecurityServiceProvider;
 use App\Providers\SaasRegistrationServiceProvider;
 
 return [
     AppServiceProvider::class,
+    AccountProfileServiceProvider::class,
     AccountSecurityServiceProvider::class,
     SaasRegistrationServiceProvider::class,
 ];
